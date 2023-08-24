68e9d6c92e5c24d9438504462b8e591a7aaf1b5f
João Pedro <JoaoPedro.stack@gmail.com>
2023-08-23 22:53:17 -0300

    initial commit

---
+++ src/main/java/com/example/SpringTest/models/ParkingSpotModel.java
@@ -0,0 +1,9 @@
+package com.example.SpringTest.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel {
+}
